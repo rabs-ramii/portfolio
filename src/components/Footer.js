1169eab2c16b760ff724ecf0cfd222b8d6095c8d
Ram Prakash Chauhan <chauhanramprakash514@gmail.com>
2023-08-13 11:51:44 +0530

    Updates

--- src/components/Footer.js
+++
@@ -1,6 +0,0 @@
-import { Link } from 'react-router-dom';
-function Footer()
-{
-    return <div className="footer">All rights reserved@2022   Designed By :<Link to='/' >rabs_ramii </Link></div>;
-}
-export default Footer;
